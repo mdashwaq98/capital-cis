9dd0baebcc0ca430e2c801a7eb9077f8ffa21ccd
mdashwaq98 <121520018+mdashwaq98@users.noreply.github.com>
2023-01-24 05:51:06 -0500

    new chg

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "testing newwww webhook.. ");
+        System.out.println( "testing newwwwqqqqq webhook.. ");
     }
 }
